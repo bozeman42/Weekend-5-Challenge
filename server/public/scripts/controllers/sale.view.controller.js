96189c8dfe97480aca239d2ecfca09c513218e3c
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-11-03 19:11:10 -0500

    sale listings are displaying on the DOM

--- server/public/scripts/controllers/sale.view.controller.js
+++ server/public/scripts/controllers/sale.view.controller.js
@@ -2,6 +2,13 @@ app.controller('SaleController', function(RealEstateService){
   console.log('SaleController created');
 
   var sc = this;
+  var rs = RealEstateService;
+  sc.result = rs.result;
+  sc.getListings = function(){
+    rs.getListings();
+  };
 
-  
+  sc.getListings();
+
+
 });
