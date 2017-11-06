0f0eecccfb705acb25c83a19a1fc19c9d1b2e847
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2017-11-06 13:16:07 -0600

    mongoose setup in module complete

---
+++ server/modules/db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var databaseUrl = 'mongodb://localhost:27017/realestate';
+
+mongoose.connection.on('connected', function(){
+  console.log('mongoose is connected');
+});
+
+mongoose.connection.on('error', function() {
+  console.log('mongoose connection failed');
+});
+
+mongoose.connect(databaseUrl,{useMongoClient: true});
+
+module.exports = mongoose;
--- server/server.js
+++ server/server.js
@@ -2,26 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var realEstate = require('./routes/realestate.router.js');
+var db = require('./modules/db.js');
 var port = process.env.PORT || 5000;
+
 
 app.use(bodyParser.json());
 app.use(express.static('server/public'));
 
 //routes
 app.use('/realestate',realEstate);
-
-var mongoose = require('mongoose');
-var databaseUrl = 'mongodb://localhost:27017/realestate';
-
-mongoose.connection.on('connected', function(){
-  console.log('mongoose is connected');
-});
-
-mongoose.connection.on('error', function() {
-  console.log('mongoose connection failed');
-});
-
-mongoose.connect(databaseUrl,{useMongoClient: true});
 
 app.listen(port, function() {
   console.log('Listening on port:', port);
